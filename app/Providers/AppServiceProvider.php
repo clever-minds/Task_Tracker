0a399272c8087199ac3ebd69ec4405c571ef5a80
Parth Soni <cleverminds.co.in@gmail.com>
2026-07-07 20:26:45 +0530

    fix: override view compilation path to write into /tmp on Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,13 @@ class AppServiceProvider extends ServiceProvider
         if (\Illuminate\Support\Facades\DB::connection() instanceof \Illuminate\Database\SQLiteConnection) {
             \Illuminate\Support\Facades\DB::statement('PRAGMA foreign_keys = ON;');
         }
+
+        if (env('VERCEL_ENV') || env('APP_ENV') === 'production') {
+            $viewPath = '/tmp/storage/framework/views';
+            if (!is_dir($viewPath)) {
+                @mkdir($viewPath, 0755, true);
+            }
+            config(['view.compiled' => $viewPath]);
+        }
     }
 }
